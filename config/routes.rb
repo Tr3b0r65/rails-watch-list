42964038b026d41b91f19f4d2937f773e3f4c518
Robert Fehlhaber <rob.fehlhaber@gmail.com>
2023-02-23 16:16:26 +0100

    files deleted

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :lists, only: [:index, :show, :new, :create] do
-    resources :bookmarks, only: [:new, :create, :destroy]
-  end
-  resources :bookmarks, only: [ :destroy ]
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+
 end
