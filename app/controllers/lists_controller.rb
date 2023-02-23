08cf6acd11b5c6a925c6b0ea5742c7ecbb5463d2
Robert Fehlhaber <rob.fehlhaber@gmail.com>
2023-02-23 14:13:17 +0100

    Seeded

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,17 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :destroy]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @bookmark = Bookmark.new
+    @review = Review.new(list: @list)
+  end
+
+  def new
+    @list = List.new
+  end
+
+  
