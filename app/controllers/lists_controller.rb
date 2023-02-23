01980975993c46f0af0e792b39646ff3144a9651
Robert Fehlhaber <rob.fehlhaber@gmail.com>
2023-02-23 16:03:48 +0100

    Files deleted and controllers re-added

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,17 +1,2 @@
 class ListsController < ApplicationController
-  before_action :set_list, only: [:show, :destroy]
-
-  def index
-    @lists = List.all
-  end
-
-  def show
-    @bookmark = Bookmark.new
-    @review = Review.new(list: @list)
-  end
-
-  def new
-    @list = List.new
-  end
-
-  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "lists#index"
-  resources :lists, except: [:edit, :update] do
-    resources :bookmarks, only: [:new, :create]
-    resources :reviews, only: :create
+  resources :lists, only: [:index, :show, :new, :create] do
+    resources :bookmarks, only: [:new, :create, :destroy]
   end
-  resources :bookmarks, only: :destroy
-  resources :reviews, only: :destroy
+  resources :bookmarks, only: [ :destroy ]
 end
